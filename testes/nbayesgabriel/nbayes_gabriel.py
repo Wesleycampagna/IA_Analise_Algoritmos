cc628ebe236a8605852ffd8d6441dac3fe823861
Wesley Campagna <wesley.campagna@hotmail.com>
2019-06-14 13:07:38 -0400

    labelEncoder, transposta, order

--- testes/nbayesgabriel/nbayes_gabriel.py
+++ testes/nbayesgabriel/nbayes_gabriel.py
@@ -9,56 +9,56 @@ from sklearn.model_selection import cross_val_score
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-	X = pd.read_csv("breast-cancer.data", sep=",", header=None)
+
+	names = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'class']
+	X = pd.read_csv("breast-cancer.data", sep=",", header=None, names=names)
+
+	features = X.columns.difference(['class'])
+
+	xx = X[features].values
+	yy = X['class'].values
+
+	xx = xx.T
 
 	le = preprocessing.LabelEncoder()
 	le.fit(["no-recurrence-events", "recurrence-events"])
-	X[0] = le.transform(X[0])
+	xx[0] = le.transform(xx[0]) 
 
 	le.fit(["10-19", "20-29", "30-39", "40-49", "50-59", "60-69", "70-79", "80-89", "90-99"])
-	X[1] = le.transform(X[1])
+	xx[1] = le.transform(xx[1])
 
 	le.fit(["lt40", "ge40", "premeno"])
-	X[2] = le.transform(X[2])
+	xx[2] = le.transform(xx[2])
 
 	le.fit(["0-4", "5-9", "10-14", "15-19", "20-24", "25-29", "30-34", "35-39", "40-44", "45-49", "50-54", "55-59"])
-	X[3] = le.transform(X[3])
+	xx[3] = le.transform(xx[3])
 
 	le.fit(["0-2", "3-5", "6-8", "9-11", "12-14", "15-17", "18-20", "21-23", "24-26", "27-29", "30-32", "33-35", "36-39"])
-	X[4] = le.transform(X[4])
+	xx[4] = le.transform(xx[4])
 
 	le.fit(["yes", "no", "?"])
-	X[5] = le.transform(X[5])
-
+	xx[5] = le.transform(xx[5])
 
 	le.fit(["left", "right"])
-	X[7] = le.transform(X[7])
-
+	xx[7] = le.transform(xx[7])
 
 	le.fit(["left_up", 'left_low', "right_up", "right_low", "central", "?"])
-	X[8] = le.transform(X[8])
+	xx[8] = le.transform(xx[8])
 
-	print(X)
-
-	le.fit(["yes", "no"])
-	X[9] = le.transform(X[9])
-
+	xx = xx.T
 	
-	#print(X)
-
-	y = X.pop(7)
+	text_file = open("output.txt", "w")
+	for el1 in range(len(xx)):
+		for el in range(len(xx[el1])):
+			if el != (len(xx[el1]) -1):
+				text_file.write(str(xx[el1][el]) + ', ')
+			else: text_file.write(str(xx[el1][el]) + ', ' + yy[el1])
+		text_file.write('\n')
+	text_file.close()
 
 	gnb = GaussianNB()
-	
-	#gs = GridSearchCV(gnb, parameters, cv = 10)
 
-	#feature_scaler = preprocessing.StandardScaler()
-
-
-	#X_train = feature_scaler.fit_transform(X)
-
-
-	all_accuracies = cross_val_score(estimator=gnb, X=X, y=y, cv=10)
+	all_accuracies = cross_val_score(estimator=gnb, X=xx, y=yy, cv=10)
 	# Acuracias de cada fold
 	print(all_accuracies)
 
